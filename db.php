1f0b27fc81fec6a04b7dc362805f8003500f1ef0
Shelby Jones <shelbyj605@gmail.com>
2024-04-18 13:15:27 -0500

    test

--- db.php
+++ db.php
@@ -1,32 +1,18 @@
+
+
 <?php
+$servername = "mysql.eecs.ku.edu";
+$username = "s352j477";
+$password = "aiCeiph7";
 
-	$conn = mysql_connect('mysql.eecs.ku.edu', '447s24_s352j477', 'aiCeiph7')
-    		or die('Could not connect: ' . mysql_error());
-	echo 'Connected successfully';
-	mysql_select_db('447s24_s352j477') or die('Could not select database');
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-// Send SQL query
-	$query = 'SELECT * FROM CRUISE';
-	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
-// Print results in HTML
-	echo "<table>\n";
-	while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    		echo "\t<tr>\n";
-    		foreach ($line as $col_value) {
-        		echo "\t\t<td>$col_value</td>\n";
-    		}
-    		echo "\t</tr>\n";
-	}
-	echo "</table>\n";
-
-	echo "Number of fields: ".mysql_num_fields($result)."<br>";
-	echo "Number of records: ".mysql_num_rows($result)."<br>";
-
-// Free resultset
-	mysql_free_result($result);
-
-// Close connection
-	mysql_close($link);
 
 ?> 
