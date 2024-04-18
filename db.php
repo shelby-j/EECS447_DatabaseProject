1dcec7deed89150f1a3516dd16cdae9a57384484
Shelby Jones <shelbyj605@gmail.com>
2024-04-18 13:38:19 -0500

    working db connection

--- db.php
+++ db.php
@@ -1,40 +1,44 @@
 
 
 <?php
-$servername = "mysql.eecs.ku.edu";
-$username = "s352j477";
-$password = "aiCeiph7";
 
-// Create connection
-$conn = new mysql_connect($servername, $username, $password);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+echo "php connection";
 
-mysql_select_db('447s24_s352j477') or die('Could not select database');
+// Connect to MySQL server, select database
+$conn = mysqli_connect('mysql.eecs.ku.edu', '447s24_s352j477', 'aiCeiph7') or die('Could not connect: ' . mysqli_error());
+echo 'Connected successfully';
+mysqli_select_db($conn, '447s24_s352j477') or die('Could not select database');
 
-// Send SQL query
-	$query = 'SELECT * FROM CRUISE';
-	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+echo 'got to db';
+
+
+$query = 'SELECT * FROM CRUISE';
+$result = mysqli_query($conn, $query, MYSQLI_STORE_RESULT) or die('Query failed: ' . mysqli_error());
+echo 'query done';
 
 // Print results in HTML
-	echo "<table>\n";
-	while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    		echo "\t<tr>\n";
-    		foreach ($line as $col_value) {
-        		echo "\t\t<td>$col_value</td>\n";
-    		}
-    		echo "\t</tr>\n";
-	}
-	echo "</table>\n";
+echo "<table>\n";
+while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+   		echo "\t<tr>\n";
+   		foreach ($line as $col_value) {
+       		echo "\t\t<td>$col_value</td>\n";
+   		}
+   		echo "\t</tr>\n";
+}
+echo "</table>\n";
+echo "Number of fields: ".mysqli_num_fields($result)."<br>";
+echo "Number of records: ".mysqli_num_rows($result)."<br>";
 
-	echo "Number of fields: ".mysql_num_fields($result)."<br>";
-	echo "Number of records: ".mysql_num_rows($result)."<br>";
+
+
+
+
+
 
 // Close connection
-mysql_close($link);
+mysqli_close($conn);
 
 ?> 
