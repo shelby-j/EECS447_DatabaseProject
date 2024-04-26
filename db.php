fac3ac9143c00d3032438cd31428070e0727e8e7
EliseLovell <112564745+EliseLovell@users.noreply.github.com>
2024-04-26 15:13:50 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,15 @@
-
-
 <?php
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+$dbName = '447s24_e016l962';
 
-$dbName = '447s24_s352j477';
-
-// Connect to MySQL server, select database
-$conn = mysqli_connect('mysql.eecs.ku.edu', $dbName, 'aiCeiph7') or die('Could not connect: ' . mysqli_error());
+//mysqli_select_db($dbName) or die('Could not select database');
+$conn = mysqli_connect('mysql.eecs.ku.edu', $dbName, 'VuVitah4')
+or die('Could not connect: ' . mysql_error());
 echo 'Connected successfully';
-mysqli_select_db($conn, $dbName) or die('Could not select database');
 
-// Close connection
+mysql_select_db($dbName) or die('Could not select database');
+
 mysqli_close($conn);
-
 ?> 
