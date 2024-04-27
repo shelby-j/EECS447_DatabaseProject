480fedc94ca5aea0120cd0302e0f583ffb0f356f
EliseLovell <112564745+EliseLovell@users.noreply.github.com>
2024-04-27 15:46:49 -0500

    Add files via upload

---
+++ newacc.php
@@ -0,0 +1,36 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$dbName = '447s24_s352j477';
+
+// Connect to MySQL server, select database
+$conn = mysqli_connect('mysql.eecs.ku.edu', '447s24_s352j477', 'aiCeiph7', '447s24_s352j477') or die('Could not connect: ' . mysqli_error());
+
+
+
+$accid = $_POST['accid'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$loc = $_POST['loc'];
+
+$sql = "SELECT * FROM ACCOUNT WHERE ACCOUNTID =\"$accid\";";
+
+$result1 = mysqli_query($conn, $sql);
+$result2 = mysqli_fetch_array($result1);
+
+
+
+if ($result2 != NULL){
+    echo("There already exists an emplyee with that ID");
+}
+else{
+    $sqldel = "INSERT INTO ACCOUNT (ACCOUNTID, FNAME, LNAME, HOMELOCATION) VALUES(\"accid\", \"fname\", \"lname\", \"loc\");";
+    $r1 = mysqli_query($conn, $sqldel);
+    
+}
+mysqli_close($conn);
+
+?>
