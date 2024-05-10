031be130dc29dc066da1bf5c286d9ab5f0aa77ae
EliseLovell <112564745+EliseLovell@users.noreply.github.com>
2024-05-10 13:55:12 -0500

    Update LoginPage.php

--- Library/LoginPage.php
+++ Library/LoginPage.php
@@ -8,6 +8,12 @@
     color: white;
     font-size: 40px;
     }
+    h2{
+    text-align: center;
+    font-family: Bookman, URW Bookman L, serif;
+    color: white;
+    font-size: 40px;
+    }
     body{
         text-align: center;
         color: white;
@@ -24,7 +30,7 @@
 
 <h1>Library Manager Login</h1>
 
-<form action = "login.php" method="post">
+<form action = "LoginPage.php" method="post">
     <?php if(isset($_GET['error'])) { ?>
         <p class="error"> <?php echo $_GET['error']; ?></p>
     <?php } ?>
@@ -36,7 +42,40 @@
   
       <input type="submit" value="Login">
   </form>
-  <h2></h2>
+  <h2>
+  <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
+$dbName = '447s24_s352j477';
+
+// Connect to MySQL server, select database
+$conn = mysqli_connect('mysql.eecs.ku.edu', '447s24_s352j477', 'aiCeiph7', '447s24_s352j477') or die('Could not connect: ' . mysqli_error());
+
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+$sql = "SELECT ENAME FROM EMPLOYEE WHERE ENAME='$username' and PASS='$password';";
+$result1 = mysqli_query($conn, $sql);
+$result2 = mysqli_fetch_array($result1);
+
+
+mysqli_close($conn);
+
+
+if ($result2 != NULL){
+	header('Location: index.html');
+	session_start();
+	$_SESSION["user"] = $username;
+}
+else{
+	echo "Invalid Username or Password"
+}
+?>
+  </h2>
 
 
 </body>
